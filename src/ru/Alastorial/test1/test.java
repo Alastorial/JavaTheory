1e411a0f0dd2599d0d96a9f72ebb14cc63fdecc2
Alastorial <alex.babykin2003@yandex.ru>
2021-10-13 02:34:28 +0300

    контроша

--- src/ru/Alastorial/test1/test.java
+++ src/ru/Alastorial/test1/test.java
@@ -6,17 +6,46 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        if (a % 10 == 1) {
-            System.out.println(a + " TOPT");
-        } else if (a > 1 && a < 5) {
-            System.out.println(a + " TOPTA");
-        } else if (a == 13) {
-            System.out.println(a + " TOPTA");
-        } else {
-            System.out.println(a + " TOPTOB");
+        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt(), m = in.nextInt(), k = 0;
+//        int[][] matrix = new int[n][m];
+//        for (int i = 0; i < n; i++) {
+//            if (i % 2 == 0) {
+//                for (int j = 0; j < m; j++) {
+//                    matrix[i][j] = k;
+//                    k++;
+//                }
+//            } else {
+//                for (int j = m - 1; j >= 0; j--) {
+//                    matrix[i][j] = k;
+//                    k++;
+//                }
+//            }
+//        }
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                System.out.print(matrix[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//        int n = in.nextInt();
+//        int a = 0;
+//        int r = 0;
+//        for (int i = 0; i < n; i++) {
+//            r = in.nextInt();
+//            if (r == 0){
+//                a += 1;
+//            } else {
+//                System.out.print(r + " ");
+//            }
+//        }
+//        for (int i = 0; i < a; i++) {
+//            System.out.print(0 + " ");
+//        }
+        int a = 2;
+        while (a < 17){
+            a *= a;
         }
+        System.out.println(a);
     }
 }
