67d679c0be69ee122af21d73dc1c7e077e54c3b3
Alastorial <alex.babykin2003@yandex.ru>
2022-09-05 11:54:25 +0300

    Create Main.java

---
+++ src/ru/TreoryBase/Les1_JDK/Main.java
@@ -0,0 +1,18 @@
+package ru.TreoryBase.Les1_JDK;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+        int[] arr = new int[2];
+//        String s = "hello %s", "world";
+        System.out.printf("hello %s%n", "world");
+        System.out.printf("hello %s", "world");
+
+
+
+
+    }
+}
